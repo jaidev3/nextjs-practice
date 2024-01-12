1919b288beb3cd5c4d88969b8f3b2671d7c8dcff
jaidev <jaidevyadav720@gmail.com>
2024-01-13 01:02:49 +0530

    basic setup done with css

---
+++ devui/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello Next js</h1>
+      <Button>Hello</Button>
+    </>
+  );
+}
